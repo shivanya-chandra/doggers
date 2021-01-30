454bc9800e06fb46bd75b7c059b917cf63618dee
shivanya <shivanya2469@gmail.com>
2021-01-30 17:59:16 +0530

    removed unwanted code

--- src/navbar.js
+++ src/navbar.js
@@ -5,27 +5,6 @@ function Navbar() {
     return (
         
     <div className="main2">
-        {/* <nav className = 'navbar'>
-                <div className='doggers'>
-                    <h2>doggers</h2>
-                </div>
-                <i class="fas fa-bars burger" ></i>
-                <a href="#" className="burger-button">
-                    <span className="bar"></span>
-                    <span className="bar"></span>
-                    <span className="bar"></span>
-                </a>
-                <div className="navbar-links">        
-            <ul>
-                <a className='link' href='/'><li>Home</li></a>
-                <a className='link' href='/funds'><li>Funds</li></a>
-                <a className='link' href='/adopt'><li>Adopt</li></a>
-                <a className='link' href='/about'><li>About</li></a>
-                <a className='link' href='#'><li>Contact</li></a>
-            </ul>
-          </div>
-
-        </nav> */}
             
             <nav className="navbar">
                 <div className="doggers">doggers</div>
@@ -38,7 +17,7 @@ function Navbar() {
                     <a className='link' href='/funds'><li>Funds</li></a>
                     <a className='link' href='/adopt'><li>Adopt</li></a>
                     <a className='link' href='/about'><li>About</li></a>
-                    {/* <a className='link' href='#'><li>Contact</li></a> */}
+                   
                 </ul>
             </nav>
 
